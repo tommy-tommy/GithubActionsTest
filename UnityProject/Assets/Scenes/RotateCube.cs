253efb02a1775b6105a1df7a2a2f323f393f3078
TommyTom19 <ntgitzzz@gmail.com>
2024-06-09 19:44:03 +0900

    add project

---
+++ UnityProject/Assets/Scenes/RotateCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    [SerializeField] float m_rotSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, m_rotSpeed * Time.deltaTime, 0);
+    }
+}
