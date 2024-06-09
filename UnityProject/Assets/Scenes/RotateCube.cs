e96857db3f8e70703205796168e7ab1871a4ecb9
TommyTom19 <ntgitzzz@gmail.com>
2024-06-09 20:03:13 +0900

    add DOTween

--- UnityProject/Assets/Scenes/RotateCube.cs
+++ UnityProject/Assets/Scenes/RotateCube.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class RotateCube : MonoBehaviour
 {
@@ -6,7 +7,13 @@ public class RotateCube : MonoBehaviour
 
     void Start()
     {
-        
+        // オブジェクトを左右に移動させる距離
+        float moveDistance = 3f;
+
+        // オブジェクトを右に移動
+        transform.DOLocalMoveX(moveDistance, 1f)
+            .SetLoops(-1, LoopType.Yoyo) // 無限ループ、Yoyoで元の位置に戻る
+            .SetEase(Ease.InOutQuad); // イージング効果で自然な動きに
     }
 
     void Update()
